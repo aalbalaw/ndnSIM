28edc72733e810a0146403dd0e8069f4912805c9
Ashok Narayanan <ashokn@cisco.com>
2013-03-13 11:31:09 -0400

    Added support for randomizing interest names and therefore lengths. Any NDN consumer can now define an additional attribute "RandComponentLenMax", and all interests generated by this consumer will have an additional randomized string component between 1 and RandComponentLenMax characters, added between the prefix and the sequence number. Producers simply ignore this element.
    Example:
    
    consumerHelper.SetPrefix ("/cp2");
    consumerHelper.SetAttribute (std::string("RandComponentLenMax"), IntegerValue(32));
    consumerHelper.Install (cp1);

--- apps/ndn-consumer.h
+++ apps/ndn-consumer.h
@@ -130,6 +130,9 @@ protected:
   NameComponents     m_interestName;        ///< \brief NDN Name of the Interest (use NameComponents)
   Time               m_interestLifeTime;    ///< \brief LifeTime for interest packet
 
+  uint32_t        m_randCompLenMax;   ///< @brief maximum length of randomly added component
+  std::string     m_randCompName;     ///< @brief string from which random component names will be built
+
 /// @cond include_hidden
   /**
    * \struct This struct contains sequence numbers of packets to be retransmitted
